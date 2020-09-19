c671e7abf50b9d3c223a8460fcff0c978b949399
sagefarrenholz <46610156+sagefarrenholz@users.noreply.github.com>
2020-09-19 19:54:12 -0400

    Added index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 const express = reqiure('express');
 const app = express();
 
+// listening port
+const port = 80;
+
+
 app.get('/api/users', (request, response) => {
-    if (request.data)
+    if (request.header
 
 })
 
 
-app.listen
+app.listen(80);
