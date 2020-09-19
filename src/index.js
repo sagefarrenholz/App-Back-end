1609a5a5f9bc6aa60ab17560425801396abed66b
sagefarrenholz <46610156+sagefarrenholz@users.noreply.github.com>
2020-09-19 19:50:47 -0400

    Fixed typo causing express to not be imported

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
-const express = reqiure('express');
+const express = require('express');
 const app = express();
 
 // listening port
 const port = 80;
 
-
 app.get('/api/users', (request, response) => {
-    if (request.header
 
-})
-
-
+});    
+    
 app.listen(80);
